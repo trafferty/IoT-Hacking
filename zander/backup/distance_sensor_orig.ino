ca616f4e349edc11ac08e4c113746459c87b7149
Tom Rafferty <thomasrafferty@gmail.com>
2020-08-17 20:18:58 -0500

    changing to state machine logic

--- zander/backup/distance_sensor_orig.ino
+++ zander/backup/distance_sensor_orig.ino
@@ -15,18 +15,15 @@ int dlay_start = 0;
 int current_ms = millis();
 int s_unknown = 0;
 int state = s_unknown;
-int D6 = 999;
-int D5 = 999;
-int D4 = 999;
-int D3 = 999;
-int D2 = 999;
-int D1 = 999;
 
-bool test = true;
-
-
-
-
+void set_lights(int pin6, int pin7, int pin8, int pin9, bool pin10)
+{
+    digitalWrite(LEDpin10, pin10);
+    digitalWrite(LEDpin9, pin9);
+    digitalWrite(LEDpin8, pin8);
+    digitalWrite(LEDpin7, pin7);
+    digitalWrite(LEDpin6, pin6);
+}
 
 
 // the setup function runs once when you press reset or power the board
@@ -45,22 +42,6 @@ void setup() {
   pinMode(LEDpin7, OUTPUT);
   pinMode(LEDpin6, OUTPUT);
 
-  if (test) {
-    D6 = 12;
-    D5 = 10;
-    D4 = 8;
-    D3 = 6;
-    D2 = 4;
-    D1 = 2;
-  } else {
-    D6 = 105;
-    D5 = 85;
-    D4 = 68;
-    D3 = 51;
-    D2 = 34;
-    D1 = 18;
-  }
-
   inches = 25;
 }
 
@@ -72,27 +53,28 @@ void loop() {
   current_ms = millis();
   //Serial.print(current_ms);
   
-  if (inches > D6)
+  if (inches > 105)
   {
     set_lights(LOW, LOW, LOW, LOW, LOW);
   }
-  else if ((inches > D5) && (inches <= D6))
+
+  else if ((inches > 85) && (inches <= 102))
   {
     set_lights(HIGH, HIGH, HIGH, HIGH, HIGH);
   }
-  else if ((inches > D4) && (inches <= D5))
+  else if ((inches > 68) && (inches <= 85))
   {
     set_lights(LOW, HIGH, HIGH, HIGH, HIGH);
   }
-  else if ((inches > D3) && (inches <= D4))
+  else if ((inches > 51) && (inches <= 68))
   {
     set_lights(LOW, LOW, HIGH, HIGH, HIGH);
   }
-  else if ((inches > D2) && (inches <= D3))
+  else if ((inches > 34) && (inches <= 51))
   {
     set_lights(LOW, LOW, LOW, HIGH, HIGH);
   } 
-  else if ((inches > D1) && (inches <= D2))
+  else if ((inches > 18) && (inches <= 34))
   {
     set_lights(LOW, LOW, LOW, LOW, HIGH);
     flash = 1;
@@ -104,43 +86,6 @@ void loop() {
 
   if ((1 == flash) && (inches <= 18))
    {
-    do_flash(dlay);
-    do_flash(dlay);
-
-    flash = 2;
-   }
-
-
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  Serial.println(inches);
-
-  delay(50);
-}
-
-void set_lights(int pin6, int pin7, int pin8, int pin9, bool pin10)
-{
-    digitalWrite(LEDpin10, pin10);
-    digitalWrite(LEDpin9, pin9);
-    digitalWrite(LEDpin8, pin8);
-    digitalWrite(LEDpin7, pin7);
-    digitalWrite(LEDpin6, pin6);
-}
-void do_flash(int dlay) {
     digitalWrite(LEDpin10, HIGH);
     delay(dlay);
     digitalWrite(LEDpin10, LOW);
@@ -179,4 +124,28 @@ void do_flash(int dlay) {
     digitalWrite(LEDpin10, HIGH);
     delay(dlay);
     digitalWrite(LEDpin10, LOW);
+
+    flash = 2;
+   }
+
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  Serial.println(inches);
+
+  delay(50);
 }

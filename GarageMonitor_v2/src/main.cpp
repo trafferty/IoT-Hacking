bb960c3d7699321a7f32c167c90f2e568639f22e
Tom Rafferty <trafferty@cnt.canon.com>
2023-07-17 20:23:21 -0500

    added NTP

--- GarageMonitor_v2/src/main.cpp
+++ GarageMonitor_v2/src/main.cpp
@@ -1,27 +1,52 @@
 #include <Arduino.h>
 
 #include <Time.h>
-// #include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <WiFiClient.h>
-// #include <ESP8266WebServer.h>
-// #include <ESP8266mDNS.h>
-#include <ArduinoOTA.h>
+//#include <ESP8266WebServer.h>
+//#include <ESP8266mDNS.h>
+//#include <ArduinoOTA.h>
 #include <EEPROM.h>
+
+#include <ArduinoOTA.h>
+#include <ESP8266WiFi.h>
+
+
+
+#include <sierra_wifi_defs.h>
 
 /*
 **  global variables...
 */
 
 // GPIO assignments
-const int in_GarageDoor = T8;
-const int out_GarageLight = T8;
-const int out_KitchenLight = T8;
-const int out_PiezoAlarm = T8;
+const int in_GarageDoor = D1;
+const int out_GarageLight = D8;
+const int out_KitchenLight = D7;
+const int out_PiezoAlarm = D6;
 const int out_HB_LED = LED_BUILTIN;
 
 unsigned long prevMillis;
 unsigned long HB_period_ms = 200;
+
+/*
+**  Network variables...
+*/
+IPAddress ip(IP1, IP2, IP3, GARAGE_MONITOR_v2_IP_LAST_FIELD);  // make sure IP is *outside* of DHCP pool range
+IPAddress gateway(GW1, GW2, GW3, GW4);
+IPAddress subnet(SN1, SN2, SN3, SN4);
+IPAddress DNS(DNS1, DNS2, DNS3, DNS4);
+const char* ssid     = SSID;
+const char* password = WIFI_PW;
+int server_port = 80;
+String DNS_name = GARAGE_MONITOR_v2_HOSTNAME;
+
+// Set web server port number
+ESP8266WebServer server(server_port);
+
+WiFiUDP Udp;
+unsigned int UdpPort = 8888;  // local port to listen for UDP packets
 
 // structs to hold start/end times for scheduling.
 tmElements_t tmCriticalStart;
@@ -37,6 +62,9 @@ char *ntpServerName = ntpServerNamePrimary;
 
 // forward declarations...
 time_t makeTimeToday(tmElements_t &tm);
+String buildDateTimeStr(time_t t);
+String buildTimeStr(time_t t);
+String formatNumber(int num);
 
 void wifi_init();
 time_t getNtpTime();
@@ -66,7 +94,7 @@ void setup()
         delay(100);
     }
 
-    // wifi_init();
+    wifi_init();
 
     // get timestamp for heartbeat LED
     prevMillis = millis();
@@ -103,33 +131,33 @@ void loop()
     criticalStart_time = makeTimeToday(tmCriticalStart);
     criticalEnd_time = makeTimeToday(tmCriticalEnd);
 
-    if (((now_time >= criticalStart_time) && (now_time <= criticalEnd_time)))
-    {
-        if (run_state == state_idle)
-        {
-            Serial.printf("Starting LED Fade program at %s\n", buildDateTimeStr(now_time).c_str());
+    // if (((now_time >= criticalStart_time) && (now_time <= criticalEnd_time)))
+    // {
+    //     if (run_state == state_idle)
+    //     {
+    //         Serial.printf("Starting LED Fade program at %s\n", buildDateTimeStr(now_time).c_str());
 
-            // setup starting config for fade state machine
-            currentR = minBrightness;
-            currentG = minBrightness;
-            currentB = maxBrightness;
-            analogWrite(BLUE_LED_OUT, currentB);
-            fade_state = blue_to_violet;
+    //         // setup starting config for fade state machine
+    //         currentR = minBrightness;
+    //         currentG = minBrightness;
+    //         currentB = maxBrightness;
+    //         analogWrite(BLUE_LED_OUT, currentB);
+    //         fade_state = blue_to_violet;
 
-            run_state = state_running;
-            force_on = false;
-        }
-    }
-    else
-    {
-        if (run_state == state_running)
-        {
-            Serial.printf("Ending LED Fade program at %s\n", buildDateTimeStr(now_time).c_str());
-            toggle_light(OFF);
+    //         run_state = state_running;
+    //         force_on = false;
+    //     }
+    // }
+    // else
+    // {
+    //     if (run_state == state_running)
+    //     {
+    //         Serial.printf("Ending LED Fade program at %s\n", buildDateTimeStr(now_time).c_str());
+    //         toggle_light(OFF);
 
-            run_state = state_idle;
-        }
-    }
+    //         run_state = state_idle;
+    //     }
+    // }
 }
 
 time_t secondsOfDay(const tmElements_t &tm)
@@ -178,4 +206,132 @@ bool insideTimePeriod(time_t currentTime, tmElements_t &tm_start, tmElements_t &
     if ((now_time >= criticalStart_time) && (now_time <= criticalEnd_time))
         insideTimePer = true;
 
+    return insideTimePer;
+}
+
+/* ----- Util functions ----- */
+void wifi_init()
+{
+  Serial.print("Setting up network with static IP.");
+  WiFi.config(ip, gateway, subnet, DNS);
+  delay(100);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.printf("Connecting to %s", ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(200);
+  }
+  Serial.println();
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Fail connecting");
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.print("WiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void digitalClockDisplay()
+{
+  // digital clock display of the time
+  Serial.println(buildDateTimeStr(now()));
+}
+
+String buildDateTimeStr(time_t t)
+{
+  String timeStr = formatNumber(hour(t));
+  timeStr += ":" + formatNumber(minute(t));
+  timeStr += ":" + formatNumber(second(t));
+  timeStr += " " + formatNumber(month(t));
+  timeStr += "_" + formatNumber(day(t));
+  timeStr += "_" + formatNumber(year(t));
+  return timeStr;
+}
+String buildTimeStr(time_t t)
+{
+  String timeStr = formatNumber(hour(t));
+  timeStr += ":" + formatNumber(minute(t));
+  timeStr += ":" + formatNumber(second(t));
+  return timeStr;
+}
+String formatNumber(int num)
+{
+  String formatted_num = "";
+  if (num < 10)
+    formatted_num = "0";
+  formatted_num += String(num);
+  return formatted_num;
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+// NTP Servers:
+const int timeZone = -5;     // Central Standard Time
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  delay(20);
+
+  uint8_t attempt = 0;
+  uint8_t maxAttempts = 3;
+  while (attempt < maxAttempts) {
+    attempt++;
+    uint32_t beginWait = millis();
+    while (millis() - beginWait < 3000) {
+      int size = Udp.parsePacket();
+      if (size >= NTP_PACKET_SIZE) {
+        Serial.println(attempt);
+        Serial.println("Receive NTP Response");
+        Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+        unsigned long secsSince1900;
+        // convert four bytes starting at location 40 to a long integer
+        secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+        secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+        secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+        secsSince1900 |= (unsigned long)packetBuffer[43];
+        return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+      }
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  ntpServerName = ntpServerNameSecondary;
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
